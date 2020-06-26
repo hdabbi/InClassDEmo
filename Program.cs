cc1556b8560cdf45a3e9645b5911f57c47ebf174
HDabbi <hemu.villi@gmail.com>
2020-06-26 17:48:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace InClassDEmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1;
+            Console.WriteLine("Your first number:");
+            try
+            {
+                Num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("My Number is: " + Num1);
+                Console.WriteLine("My New Number is:" + Num1+33);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+                 
+        }
+    }
+}
